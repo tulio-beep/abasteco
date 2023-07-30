669d6e9826c91928b9f56120583819205c511111
tulio-beep <tuliovin@outlook.com>
2023-07-30 12:58:07 -0300

    2commit

--- index.js
+++ index.js
@@ -1,27 +1,78 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors"); // Importe o módulo cors
+const fs = require("fs");
 
 const app = express();
-const port = process.env.port;
+const port = 3000;
 
-// Configuração para analisar o corpo das requisições como JSON
 app.use(bodyParser.json());
 
-// Rota para receber as coordenadas do usuário
-app.post("/coordenadas", (req, res) => {
+// Configuração do CORS
+app.use(cors());
+
+app.post("/coordenadas", async (req, res) => {
   const { latitude, longitude } = req.body;
-  // Verifique se as coordenadas foram fornecidas corretamente
+
   if (latitude === undefined || longitude === undefined) {
     return res.status(400).json({ error: "Coordenadas inválidas." });
   }
   console.log(latitude, longitude);
-  // Aqui, você pode fazer o que quiser com as coordenadas.
-  // Por exemplo, pode retornar uma mensagem com base nas coordenadas recebidas.
-  // Neste exemplo, vamos apenas devolver as coordenadas recebidas.
-  return res.json({ latitude, longitude });
+  data = fs.readFileSync("data.Json");
+  data = JSON.parse(data);
+  for (let i = 0; i < data.length; i++) {
+    data[i].distance = haversineDistance(
+      latitude,
+      longitude,
+      data[i].Latitude,
+      data[i].Longitude
+    );
+  }
+  data = await data.filter((a) => a.distance <= 50);
+  console.log(data);
+  return res.json(data);
 });
 
-// Iniciando o servidor
+app.get("/home", (req, res) => {
+  const filePath = "index.html";
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Erro ao ler o arquivo:", err);
+      res.status(500).send("Erro interno do servidor");
+    } else {
+      res.setHeader("Content-Type", "text/html");
+      res.send(data);
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+function toRadians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+// Função para calcular a distância entre dois pontos em latitude e longitude
+function haversineDistance(lat1, lon1, lat2, lon2) {
+  const earthRadiusKm = 6371; // Raio médio da Terra em quilômetros
+
+  // Converter as coordenadas de graus para radianos
+  const dLat = toRadians(lat2 - lat1);
+  const dLon = toRadians(lon2 - lon1);
+  const radLat1 = toRadians(lat1);
+  const radLat2 = toRadians(lat2);
+
+  // Aplicar a fórmula de Haversine
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) *
+      Math.sin(dLon / 2) *
+      Math.cos(radLat1) *
+      Math.cos(radLat2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = earthRadiusKm * c;
+
+  return distance;
+}
--- recive_data.js
+++ recive_data.js
@@ -1,5 +1,9 @@
 const axios = require("axios");
 const res = require("express/lib/response");
+const fs = require("fs");
+const csv = require("csv-parser");
+const { stringify } = require("querystring");
+const { Readable } = require("stream");
 
 urls = [];
 
@@ -23,10 +27,13 @@ for (i = 1; i < 13; i++) {
     y_ = y;
   }
   urls.push(
-    `https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/pmqc/${y_}/pmqc-${m_}.json`
+    `https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/pmqc/${y_}/pmqc-${m_}.csv`
   );
   urls.push(
-    `https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/pmqc/${y_}/pmqc_${y_}_${m_}.json`
+    `https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/pmqc/${y_}/pmqc_${y_}_${m_}.csv`
+  );
+  urls.push(
+    `https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/pmqc/${y_}/${y_}-${m_}-pmqc.csv`
   );
 }
 
@@ -38,7 +45,7 @@ function getJsonFromUrl(url) {
           `Erro na solicitação da URL ${url}. Código de status: ${response.status}`
         );
       }
-      return response.json();
+      return response;
     })
     .catch((error) => {
       console.error(`Erro ao acessar a URL ${url}: ${error}`);
@@ -62,16 +69,89 @@ async function obterJsonsEmParalelo() {
     const promessasJson = urls.map(getJsonFromUrl);
     // Promise.all aguarda todas as promessas serem resolvidas e retorna um vetor com os resultados
     const resultados = await Promise.all(promessasJson);
-    m = 0;
-    for (i = 0; i < resultados.length; i++) {
-      if (resultados[i] != null) {
-        data.push(resultados[i]);
-      }
-    }
-    console.log(data);
+
+    data = await concatCSVData(resultados);
+
+    data = await data.filter((a) => a.Ensaio == "Teor de Etanol");
+    data = await data.sort((a, b) => b.DataColeta.localeCompare(a.DataColeta));
+    data = await onlyUnique(data);
+    data = data.filter((a) => validaAlcool(a.Resultado));
+    fs.writeFileSync("data.Json", JSON.stringify(data));
     // Agora você pode fazer o que quiser com o vetor de resultados
-  } catch (error) {}
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 // Chamando a função para obter os JSONs em paralelo
 obterJsonsEmParalelo();
+
+async function concatCSVData(resultados) {
+  let data = [];
+
+  for (let i = 0; i < resultados.length; i++) {
+    if (resultados[i] != null) {
+      const processedData = await processCSVData(resultados[i]);
+      data = data.concat(processedData);
+    }
+  }
+
+  return data;
+}
+
+function responseToReadable(responseData) {
+  const readableStream = new Readable();
+  readableStream._read = () => {}; // A função vazia é necessária para que o stream funcione corretamente
+  readableStream.push(responseData);
+  readableStream.push(null);
+  return readableStream;
+}
+
+// Função para processar os dados do CSV e retornar como uma resposta (Promise)
+function processCSVData(csvData) {
+  return new Promise((resolve, reject) => {
+    const results = [];
+
+    responseToReadable(csvData) // Transformar a resposta em um stream
+      .pipe(csv({ separator: ";" }))
+      .on("data", (data) => results.push(data))
+      .on("end", () => {
+        // Aqui, a leitura do CSV foi concluída, e os dados estão armazenados em "results"
+        resolve(results); // Resolvendo a Promise com os dados do CSV
+      })
+      .on("error", (error) => {
+        reject(error); // Rejeitando a Promise em caso de erro
+      });
+  });
+}
+
+function onlyUnique(arr) {
+  // Verifica se a entrada é uma array válida
+  if (!Array.isArray(arr)) {
+    throw new TypeError("A entrada deve ser uma array.");
+  }
+
+  // Cria um objeto para rastrear a contagem de cada "CnpjPosto" na array
+  const cnpjCount = {};
+
+  // Loop para contar a ocorrência de cada "CnpjPosto" na array
+  for (const obj of arr) {
+    if (!obj || typeof obj !== "object" || !obj.hasOwnProperty("CnpjPosto")) {
+      throw new TypeError(
+        'A entrada deve ser uma array de objetos com a propriedade "CnpjPosto".'
+      );
+    }
+
+    const cnpj = obj.CnpjPosto;
+    cnpjCount[cnpj] = (cnpjCount[cnpj] || 0) + 1;
+  }
+
+  // Filtra a array original, incluindo apenas os objetos com "CnpjPosto" único
+  const uniqueArr = arr.filter((obj) => cnpjCount[obj.CnpjPosto] === 1);
+
+  return uniqueArr;
+}
+
+function validaAlcool(alcool) {
+  return alcool >= 26 && alcool <= 28;
+}
